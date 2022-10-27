01efd25387f1288e5c02f654f95bcb815b06d8e3
Daniel <dbecerrapa@unbosque.edu.co>
2022-10-26 20:34:05 -0500

    modificados

--- Graficas.R
+++ Graficas.R
@@ -1,7 +1,8 @@
-fecha_i<-as.Date("2022-05-01")
-fecha_f<-as.Date("2022-06-30")
-cancelacion <- "NO"
-especialidad<- "NA"
+#fecha_i<-as.Date("2022-05-02")
+#fecha_f<-as.Date("2022-09-30")
+#date_range<-fecha_i:fecha_f
+#cancelacion <- "NO"
+#especialidad<- "NA"
 
 
 if(cancelacion == "NA"){
@@ -16,11 +17,10 @@ if(especialidad == "NA"){
   base_2<-base_1 %>% filter(`ESPECIALIDAD TRATANTE`==especialidad)
 }
 
-
 # CONTADOR DE PACIENTES ---------------------------------------------------
 
 base_3<- base_2 %>% 
-  filter(`FECHA DE CIRUGIA`>=fecha_i & `FECHA DE CIRUGIA`<= fecha_f) %>% 
+  filter(`FECHA DE CIRUGIA`>=fecha_i & `FECHA DE CIRUGIA`<=fecha_f) %>% 
   select(`FECHA DE CIRUGIA`, `SALA DE CIRUGIA`,`PROCEDIMIENTO 1`,`TIPO DE ANESTESIA`) 
 n_ptes<- nrow(base_3)
 n_ptes
--- ResumenVirrey/ui.R
+++ ResumenVirrey/ui.R
@@ -6,21 +6,27 @@ ui <- fluidPage(
   
   titlePanel("Estadistica UMEQ calle 100"),
   
-  dateRangeInput(inputId = "datarange", label = "Rango: ", start = "2022-05-01", end = "2022-09-30"
+  dateRangeInput(inputId = "datarange", 
+                 label = "Rango: ", 
+                 start = "2022-05-01", 
+                 end = "2022-05-31",
                  ),
-  selectInput(inputId = "cancelacion", label = "¿Procedimiento cancelado?",choices = c("SI","NO","NA"),selected = "NA"
+  selectInput(inputId = "cancelacion", 
+              label = "¿Procedimiento cancelado?",
+              choices = c("SI","NO","NA"),
+              selected = "NA"
               ),
-  selectInput(inputId = "especialidad", label = "Seleccione especialidad: ", choices = unique(base$`ESPECIALIDAD TRATANTE`),selected = "NA"
+  selectInput(inputId = "especialidad", 
+              label = "Seleccione especialidad: ", 
+              choices = unique(base$`ESPECIALIDAD TRATANTE`),
+              selected = "NA"
               ),
   plotOutput(outputId = "plot_pte"),
   
   plotOutput(outputId = "plot_esp"),
   
-  textOutput(outputId = "num_pte")
-  
-  
-  
-  
-              
+  flowLayout(
+    textOutput(outputId = "num_pte")
   )
+)
 
